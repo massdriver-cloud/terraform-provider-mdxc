b3c61099cd3bd56ffbd06cbcf749cb1c307cf5bf
Cory O'Daniel <cory@coryodaniel.com>
2022-08-11 03:10:29 -0700

    wip

--- mdxc/internal/gcp/app_identity/main_test.go
+++ mdxc/internal/gcp/app_identity/main_test.go
@@ -2,44 +2,40 @@ package app_identity_test
 
 import (
 	"context"
+	"fmt"
 	"terraform-provider-mdxc/mdxc/internal/gcp/app_identity"
 	"terraform-provider-mdxc/mdxc/internal/massdriver"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"google.golang.org/api/googleapi"
 	"google.golang.org/api/iam/v1"
 )
 
-type mockProjectsServiceAccountsCreateCall iam.ProjectsServiceAccountsCreateCall
+// type mockProjectsServiceAccountsCreateCall iam.ProjectsServiceAccountsCreateCall
 
-func (m *mockProjectsServiceAccountsCreateCall) Do(opts ...googleapi.CallOption) (*iam.ServiceAccount, error) {
-	return &iam.ServiceAccount{}, nil
+// func (m *mockProjectsServiceAccountsCreateCall) Do(opts ...googleapi.CallOption) (*iam.ServiceAccount, error) {
+// 	return &iam.ServiceAccount{}, nil
+// }
+
+// type mockCreateServiceAccountAPI func(projectName string, createserviceaccountrequest *iam.CreateServiceAccountRequest) *mockProjectsServiceAccountsCreateCall
+
+// func (m mockCreateServiceAccountAPI) Create(projectName string, createserviceaccountrequest *iam.CreateServiceAccountRequest) *mockProjectsServiceAccountsCreateCall {
+// 	return &mockProjectsServiceAccountsCreateCall{}
+// }
+
+type mockCreateServiceAccountAPI struct{}
+
+func (m mockCreateServiceAccountAPI) Create(projectName string, createserviceaccountrequest *iam.CreateServiceAccountRequest) *iam.ProjectsServiceAccountsCreateCall {
+	fmt.Printf("I am being called")
+	return &iam.ProjectsServiceAccountsCreateCall{}
 }
 
-type mockCreateServiceAccountAPI func(projectName string, createserviceaccountrequest *iam.CreateServiceAccountRequest) *mockProjectsServiceAccountsCreateCall
-
-func (m mockCreateServiceAccountAPI) Create(projectName string, createserviceaccountrequest *iam.CreateServiceAccountRequest) *mockProjectsServiceAccountsCreateCall {
-	return &mockProjectsServiceAccountsCreateCall{}
-}
-
-func CreateServiceAccount(t *testing.T) {
-	m := mockCreateServiceAccountAPI(func(projectName string, createserviceaccountrequest *iam.CreateServiceAccountRequest) *mockProjectsServiceAccountsCreateCall {
-		t.Helper()
-		// if params.RoleName == nil {
-		// 	t.Fatal("expect role name to not be nil")
-		// }
-
-		// arn := fmt.Sprintf("arn:aws:iam::account:role/%s", *params.RoleName)
-		// role := &types.Role{Arn: aws.String(arn)}
-		// return &iam.CreateRoleOutput{Role: role}, nil
-		mockCreateCall := mockProjectsServiceAccountsCreateCall{}
-		return &mockCreateCall
-	})
-
+func TestCreateServiceAccount(t *testing.T) {
 	appIdentityInput := massdriver.AppIdentityInput{
 		Name: aws.String("test"),
 	}
+
+	m := &mockCreateServiceAccountAPI{}
 
 	serviceAccount, _ := app_identity.CreateServiceAccount(context.TODO(), m, &appIdentityInput)
 	got := serviceAccount.Email
